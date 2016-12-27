33757512dd37f2838312aa7f78434467672e8e17
Elliot Bouvier <elliotbouvier@yahoo.fr>
2016-12-27 20:45:29 +0400

    refactor

--- script/card.ts
+++ script/card.ts
@@ -5,21 +5,36 @@
 
 export class Card {
 
+    protected static _increment = 0;
+
     private _name: string;
+    private _layout: any;
 
-    constructor(name:string) {
+    constructor(name: string) {
         this._name = name;
+        this._layout = document.getElementById('layout');
+        Card._increment += 1;
     }
 
-    public show() {
-        let layout = document.getElementById('layout').innerHTML += `<img src="./img/${this._name}.jpg">`
+    public init(): void {
+        let card =
+            `<div class="col-xs-3">
+                <div class="noir" id="${this._name }${Card._increment}"></div>
+                <img src="./img/${this._name}.jpg" id="${this._name}">
+            </div>`;
+        this._layout.append(card);
+        //document.getElementById(this._name + Card._increment).onclick = this.click;
     }
 
-    public hide() {
-
+    public show(card: any) {
+        return card.className = ''
     }
 
-    public click() {
+    public hide(card: any): string {
+        return card.className = 'noir'
+    }
 
+    public click(name: any): any {
+        console.log(name);
     }
 }
--- script/game.ts
+++ script/game.ts
@@ -1,7 +1,7 @@
 /**
-    * Created by elliotbouvier on 12/27/16.
-    * challenge-js-jeu-des-paires
-    */
+ * Created by elliotbouvier on 12/27/16.
+ * challenge-js-jeu-des-paires
+ */
 
 import {Timer} from './timer';
 import {Card} from './card';
@@ -10,22 +10,24 @@ class Game {
 
 
     constructor() {
+
     }
 
     public start() {
         let timer = new Timer();
-        let cardName = this.random(['ane', 'chat', 'chien', 'lama', 'lapins', 'lionne', 'ours']);
-        console.log(cardName);
-        console.log(cardName.forEach((e) => new Card(e).show()))
+        let set1 = this.random(['ane', 'chat', 'chien', 'lama', 'lapins', 'lionne', 'ours']);
+        let set2 = this.random(['ane', 'chat', 'chien', 'lama', 'lapins', 'lionne', 'ours']);
+        set1.forEach((e) => new Card(e).init())
+        set2.forEach((e) => new Card(e).init())
 
     }
 
-    public  random(imageArray: string[]): string[]{
+    public  random(imageArray: string[]): string[] {
         return _.shuffle(imageArray);
     }
 
     public compareCards() {
-
+        let clickedCard = document.getElementsByClassName('hidden');
     }
 
     public end() {
@@ -33,5 +35,5 @@ class Game {
     }
 }
 
-let game = new Game();
+const game = new Game();
 game.start();
